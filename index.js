fe93fc20ff3bad4b174777fd5cb888200c44bc90
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2023-12-22 18:09:21 -0800

    Get Single data by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -44,12 +44,27 @@ async function run() {
       res.send(result);
     });
 
+    app.delete('/deleteTask/:id',async(req,res)=>{
+        const id = req.params.id;
+        // console.log(id);
+        const query = {_id : new ObjectId(id)}
+        const result = await taskCollection.deleteOne(query)
+        res.send(result);
+    })
+
+    app.get('/getSingleTask/:id',async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id : new ObjectId(id)}
+      const result = await taskCollection.findOne(query)
+      res.send(result); 
+    })
 
 
 
 
 
-    
+
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
